b454afaea2b6a064c680f282b5bd79794c89e5b8
Nelson <nelson@ccb.wtf>
2019-09-09 10:26:04 +0800

    Trying is st data can be persistent

--- siteHandler/straitsTimesHandler.js
+++ siteHandler/straitsTimesHandler.js
@@ -1,15 +1,8 @@
 const puppeteer = require("puppeteer");
-const CREDS = require("../creds/straitsTimes");
-
-const LOGIN_PAGE = "https://acc-reg.sphdigital.com/RegAuth2/gdsLogin.html";
-const USERNAME_SELECTOR = "#j_username";
-const PASSWORD_SELECTOR =
-  "#loginForm > ol:nth-child(1) > li:nth-child(5) > input:nth-child(1)";
-const BUTTON_SELECTOR = "button.formbutton:nth-child(1)";
 
 async function straitsTimesHandler(url) {
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: !true,
     defaultViewport: { height: 736, width: 414 },
     args: ["--no-sandbox"],
     userDataDir: "./st_data"
@@ -22,27 +15,15 @@ async function straitsTimesHandler(url) {
       "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
     );
 
-    await page.goto(LOGIN_PAGE);
-
-    await page.click(USERNAME_SELECTOR);
-    await page.keyboard.type(CREDS.username);
-
-    await page.click(PASSWORD_SELECTOR);
-    await page.keyboard.type(CREDS.password);
-
-    await Promise.all([
-      page.waitForNavigation({ waitUntil: "networkidle0" }),
-      page.click(BUTTON_SELECTOR)
-    ]);
-
     try {
       await Promise.all([
-        page.waitForNavigation({ timeout: 10000 }),
+        page.waitForNavigation({ waitUntil: "networkidle2" }),
         page.goto(url)
       ]);
     } catch (e) {}
 
     // Disable Javascript so weird overlays can't be created
+    /*
     await page.setJavaScriptEnabled(false);
 
     await page.reload();
@@ -51,11 +32,12 @@ async function straitsTimesHandler(url) {
     await page.emulateMedia("screen");
 
     await page.pdf({ path: "article.pdf", width: 414, height: 736 });
+    */
   } catch (e) {
     await page.pdf({ path: "error.pdf", width: 414, height: 736 });
     throw e;
   } finally {
-    browser.close();
+    // browser.close();
   }
 }
 
