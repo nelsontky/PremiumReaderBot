5b695dbfc00d39aa260dcd61e6ea8b1d49f32938
Nelson <nelson@ccb.wtf>
2019-11-13 01:27:08 +0800

    Fixed WSJ

--- siteHandler/genericHandler.js
+++ siteHandler/genericHandler.js
@@ -7,10 +7,10 @@ async function genericHandler(url, domain) {
   const options = {
     uri: url,
     headers: {
-      "User-Agent": "Googlebot-News"
-    },
-    referrer:
-      "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=4&cad=rja&uact=8"
+      "User-Agent":
+        "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+      Referer: "https://www.google.com/"
+    }
   };
 
   const html = await rp(options);
@@ -23,11 +23,20 @@ async function genericHandler(url, domain) {
     case "wsj.com":
       title = $("title", html).text();
       $(".article-content > p", html).each((i, e) => {
-        body += $(e).text() + "\n";
+        body += $(e)
+          .text()
+          .replace(/(\r\n|\n|\r)/g, "");
+        body += "\n";
       });
       $(".paywall > p", html).each((i, e) => {
-        body += $(e).text() + "\n";
+        body += $(e)
+          .text()
+          .replace(/(\r\n|\n|\r)/g, "");
+        body += "\n";
       });
+      try {
+        image = $("img", html)[0].attribs.src;
+      } catch (e) {}
       return await postToTelegraph(title, body, image);
 
     case "nytimes.com":
